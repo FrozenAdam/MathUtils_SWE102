db03f91288866bcf538dd58012a5bc58b2079c37
FrozenAdam <phamlephucanh@yahoo.com.vn>
2020-07-20 07:16:24 +0700

    4th: Add exception handler

--- src/anhplp/util/MathUtils.java
+++ src/anhplp/util/MathUtils.java
@@ -12,9 +12,13 @@ package anhplp.util;
 //class chua cac ham toan hoc tinh toan voi muc tieu la xai chung cho cac noi khac
 //cai gi xai chung thi mac dinh la static
 public class MathUtils {
+
     //vi la tinh so giai thua => dung kieu long
     //tinh giai thua tu 15! tro ve sau
     public static long computeFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Invalid input, n must larger than 0");
+        }
         long result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
